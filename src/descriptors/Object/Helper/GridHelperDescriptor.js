4f6dd7b6eaa57e5de7d25d249771e115f53a2c7f
Stephen Austin <stephen.austin@pwnt.co>
2015-12-18 21:22:57 -0500

    added grid helper

---
+++ src/descriptors/Object/Helper/GridHelperDescriptor.js
@@ -0,0 +1,37 @@
+import THREE from 'three.js';
+import Object3DDescriptor from '../Object3DDescriptor';
+import PropTypes from 'react/lib/ReactPropTypes';
+
+class GridHelperDescriptor extends Object3DDescriptor {
+  constructor(react3Instance) {
+    super(react3Instance);
+
+    this.propTypes = {
+      ...this.propTypes,
+
+      size: PropTypes.number,
+      step: PropTypes.number,
+    };
+  }
+
+  applyInitialProps(threeObject:THREE.Object3D, props) {
+    super.applyInitialProps(threeObject, props);
+  }
+
+  construct(props) {
+    const {
+      size,
+      step,
+      } = props;
+    return new THREE.GridHelper(size, step);
+  }
+
+  unmount(threeObject) {
+    threeObject.geometry.dispose();
+    threeObject.material.dispose();
+
+    super.unmount(threeObject);
+  }
+}
+
+export default GridHelperDescriptor;
